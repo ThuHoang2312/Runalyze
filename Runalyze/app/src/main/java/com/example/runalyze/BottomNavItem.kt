0a80e1636c5cdfea271e35c105f0b5297e9ce2b2
Chi Nguyen <chi.nguyen@live.com>
2023-09-26 02:10:10 +0300

    add bottom navigation bar

---
+++ Runalyze/app/src/main/java/com/example/runalyze/BottomNavItem.kt
@@ -0,0 +1,15 @@
+package com.example.runalyze
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.DirectionsRun
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.QueryStats
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomNavItem(val route: String, val icon: ImageVector, val title: String){
+    object Home: BottomNavItem("home", icon = Icons.Outlined.Home, title = "Home")
+    object Training: BottomNavItem("training", icon = Icons.Outlined.DirectionsRun, title = "Training")
+    object Activity: BottomNavItem(route = "activity", icon = Icons.Outlined.QueryStats, title = "Activity")
+    object Profile: BottomNavItem(route = "profile", icon = Icons.Outlined.Person, title = "Profile")
+}
---
+++ Runalyze/app/src/main/java/com/example/runalyze/components/BottomNavigationMenu.kt
@@ -0,0 +1,50 @@
+package com.example.runalyze.components
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.colorResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.runalyze.BottomNavItem
+import com.example.runalyze.R
+
+@Composable
+fun BottomNavigationMenu(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Training,
+        BottomNavItem.Activity,
+        BottomNavItem.Profile)
+
+
+        NavigationBar(contentColor = colorResource(id = R.color.white)) {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+            items.forEach {
+                NavigationBarItem(
+                    label = {
+                        Text(text = it.title)
+                    },
+                    alwaysShowLabel = true,
+                    selected = currentRoute == it.route,
+                    onClick = {
+                        navController.navigate(it.route) {
+                            navController.graph.startDestinationRoute?.let { route ->
+                                popUpTo(route) {
+                                    saveState = true
+                                }
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = { Icon(imageVector = it.icon, contentDescription = it.title) }
+                )
+            }
+        }
+
+}
